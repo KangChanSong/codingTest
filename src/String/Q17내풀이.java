9babb2b13a26adf5c2947ae920939b3373bfe2c8
KangChanSong <thdrkdcks@naver.com>
2021-07-25 22:04:07 +0900

    Q17_시간초과

---
+++ src/String/Q17내풀이.java
@@ -0,0 +1,37 @@
+package String;
+
+import java.util.Scanner;
+
+//시간초과..
+public class Q17내풀이 {
+
+    static int solution(int n){
+
+        int count = 0;
+        boolean flag = false;
+
+        for(int i=2; i <= n ; i++){
+
+            for(int j=2 ; j< i ; j++){
+                if(i%j == 0) flag = true;
+            }
+
+            if(!flag){
+                count++;
+            } else{
+                flag = false;
+            }
+
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int num = scanner.nextInt();
+        System.out.println(solution(num));
+    }
+}
