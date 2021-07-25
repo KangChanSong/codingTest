867cd14137819550fba54f3b448c73836a0e6dda
KangChanSong <thdrkdcks@naver.com>
2021-07-25 18:01:43 +0900

    5문제

---
+++ src/String/Q12.java
@@ -0,0 +1,29 @@
+package String;
+
+import java.util.Scanner;
+
+public class Q12 {
+
+    static String solution(int length, String string){
+
+        String result = "";
+        for(int i = 0 ; i < length ; i++){
+            String s = string.substring(0, 7).replace("#", "1").replace("*", "0");
+            int num = Integer.valueOf(s , 2);
+            result += (char) num;
+
+            string = string.substring(7);
+        }
+
+        return result;
+    };
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        String s = scanner.next();
+
+        System.out.println(solution(num, s));
+    }
+}
---
+++ src/String/Q13.java
@@ -0,0 +1,31 @@
+package String;
+
+import java.util.Scanner;
+
+public class Q13 {
+
+    static void solution(int[] arr){
+
+        for(int i=0 ; i < arr.length ; i++){
+            if(i == 0){
+                System.out.print(arr[i] + " ");
+            } else if( arr[i-1] < arr[i]){
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int rep = scanner.nextInt();
+
+        int[] param = new int[rep];
+
+        for(int i = 0 ; i < rep ; i++){
+            param[i] = scanner.nextInt();
+        }
+
+        solution(param);
+    }
+}
