a51908350288dce55320857ddf80783a27e963b9
KangChanSong <thdrkdcks@naver.com>
2021-10-31 13:12:11 +0900

    bfs 순회 구현

--- src/Inflearn/treegraph/BTreeTraverse.java
+++ src/Inflearn/treegraph/BTreeTraverse.java
@@ -1,5 +1,8 @@
 package Inflearn.treegraph;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BTreeTraverse {
 
     public static void main(String[] args) {
@@ -16,6 +19,11 @@ public class BTreeTraverse {
         bTree.reset();
         System.out.println("=== postodrer ===");
         bTree.postorder();
+        System.out.println();
+
+        bTree.reset();
+        System.out.println("=== BFS ===");
+        bTree.bfs();
     }
 
     static class BTree{
@@ -87,6 +95,19 @@ public class BTreeTraverse {
             if(node.isMarked == false){
                 System.out.print(node.data + " ");
                 node.isMarked = true;
+            }
+        }
+
+        void bfs(){
+            Queue<Node> queue = new LinkedList<>();
+            queue.add(root);
+
+            while (!queue.isEmpty()){
+                Node node = queue.poll();
+                System.out.print(node.data + " ");
+                node.isMarked = true;
+                if(node.left != null) queue.add(node.left);
+                if(node.right != null) queue.add(node.right);
             }
         }
     }
