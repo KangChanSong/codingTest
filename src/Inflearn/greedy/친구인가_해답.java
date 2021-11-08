fbb7fb4113938ba98d046a2d0555b80f3a115696
KangChanSong <thdrkdcks@naver.com>
2021-11-08 19:31:04 +0900

    원더랜드

--- src/Inflearn/greedy/친구인가_해답.java
+++ src/Inflearn/greedy/친구인가_해답.java
@@ -4,26 +4,16 @@ import java.util.Scanner;
 
 // 서로소 집합을 만들어 두 학생이 같은 집합 안에 있으면 YES 아니면 NO 를 반환하도록 작성
 public class 친구인가_해답 {
-    static int[] unf; // 서로소 집합을 표현할 배열
-
-    static int find(int v){ // v 학생의 집합 번호를 리턴해준다.
-        if(v == unf[v]) return v;
-        return unf[v] = find(unf[v]);
-    }
-
-    static void union(int a, int b){
-        int fa = find(a);
-        int fb = find(b);
-        if(fa != fb) unf[fa] = fb;
-    }
+    static int[] unf;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int m = scanner.nextInt();
         unf = new int[n+1];
-        for(int i = 1; i <= n ; i++) unf[i] = i;
-        for(int i = 1 ; i <= m ; i++){
+        for(int i = 0 ; i <= n; i++) unf[i] = i;
+
+        for(int i = 0 ; i < m ; i++){
             int a = scanner.nextInt();
             int b = scanner.nextInt();
             union(a, b);
@@ -34,5 +24,16 @@ public class 친구인가_해답 {
         int fa = find(a);
         int fb = find(b);
         System.out.println(fa == fb ? "YES" : "NO");
+    }
+
+    static int find(int x){
+        if(x == unf[x]) return x;
+        else return find(unf[x]);
+    }
+
+    static void union(int a, int b) {
+        int fa = find(a);
+        int fb = find(b);
+        if(fa != fb) unf[fa] = fb;
     }
 }
